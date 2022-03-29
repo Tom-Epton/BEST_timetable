518aed10c039d999cb9d904918e25f4720a32844
Tom-Epton <65612739+Tom-Epton@users.noreply.github.com>
2022-03-29 22:33:01 +0200

    Run sheet to mat in server

--- web_server.py
+++ web_server.py
@@ -29,6 +29,7 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            os.system('python3 sheet_to_matrix.py')
             return redirect(url_for('download_file', name=filename))
     return '''
     <!doctype html>
